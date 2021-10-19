3c1b629ac3ad677ba9ba155a66f38257042d844d
LESLEE WENDOLYNE GARCIA TREJO <I21050589@monclova.tecnm.mx>
2021-10-19 15:45:27 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1Arreglo/Calificaciones.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArreglo
+{
+    class Calificaciones
+    {
+        private string nombre;
+        private string apellidos;
+        private string promedio;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        public string Promedio
+        {
+            get { return promedio; }
+            set { promedio = value; }
+        }
+        public Calificaciones()
+        {
+            nombre = "";
+            apellidos = "";
+            Promedio = "";
+        }
+        public Calificaciones(string nombre, string apellidos, string carrera)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.promedio = carrera;
+        }
+        public override string ToString()
+        {
+            return nombre + " " + apellidos + " " + promedio + " ";
+        }
+
+    }
+}
---
+++ ConsoleApp1Arreglo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleAppArreglo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calificaciones a = new Calificaciones();
+            Calificaciones[] arregloCalificaciones = new Calificaciones[5];
+            for (int i = 0; i < arregloCalificaciones.Length; i++)
+            {
+                arregloCalificaciones[i] = new Calificaciones();
+                Console.WriteLine(" Nombre");
+                arregloCalificaciones[i].Nombre = "";
+                arregloCalificaciones[i].Nombre = Console.ReadLine();
+                Console.WriteLine("Apellidos");
+                arregloCalificaciones[i].Apellidos = "";
+                arregloCalificaciones[i].Apellidos = Console.ReadLine();
+                Console.WriteLine("Promedio");
+                arregloCalificaciones[i].Promedio = "";
+                arregloCalificaciones[i].Promedio = Console.ReadLine();
+            }
+            for (int i = 0; i < arregloCalificaciones.Length; i++)
+            {
+                Console.WriteLine(arregloCalificaciones[i].ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
